ecdaa9e5a10546ec0730fea825e1d7ce0d9850c8
Anand.G.Murugan <anandgm9000@gmail.com>
2023-06-20 18:25:16 +0530

    Update deletor.js

---
+++ deletor.js
@@ -0,0 +1,8 @@
+javascript:(function() 
+            {  
+              var elements = document.querySelectorAll('div.Kp2okb.SQyOec[jsname="Iq1FLd"][style="flex-basis: 225px;min-width: 225px;"], div#cookieBar');  
+              elements.forEach(function(element) {    
+                element.parentNode.removeChild(element);  
+              });
+            })
+  ();
